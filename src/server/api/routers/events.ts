7cc9a01bd11192faf89118634ee96f290aa5425b
Krish <52761222+Krish120003@users.noreply.github.com>
2023-03-04 17:31:05 -0500

    add landing

---
+++ src/server/api/routers/events.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+
+export const eventsRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        location: z.string(),
+        slug: z.string(),
+        startTime: z.date(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+        await ctx.prisma.event.create(
+            {
+                data: {
+                    name: input.name,
+                    description: input.description,
+                    location: input.location,
+                    slug: input.slug,
+                    startTime: input.startTime,
+                    creatorId: ctx.session.user.id
+                }
+            }
+        );
+
+      return "Success"
+    }),
+
+    get: publicProcedure.input(z.string()).query(async ({input: slug, ctx}) => {
+        return await ctx.prisma.event.findUniqueOrThrow({
+            where: {
+                slug: slug
+            }
+        })
+    })
+});
