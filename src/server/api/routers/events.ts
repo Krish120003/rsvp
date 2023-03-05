610630b58c4f498df37d17b084bf376dd695df94
Krish <52761222+Krish120003@users.noreply.github.com>
2023-03-05 01:39:55 -0500

    add count as well

--- src/server/api/routers/events.ts
+++ src/server/api/routers/events.ts
@@ -14,30 +14,29 @@ export const eventsRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input, ctx }) => {
-        await ctx.prisma.event.create(
-            {
-                data: {
-                    name: input.name,
-                    description: input.description,
-                    location: input.location,
-                    slug: input.slug,
-                    startTime: input.startTime,
-                    creatorId: ctx.session.user.id
-                }
-            }
-        );
-      return "Success"
+      await ctx.prisma.event.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          location: input.location,
+          slug: input.slug,
+          startTime: input.startTime,
+          creatorId: ctx.session.user.id,
+        },
+      });
+      return "Success";
     }),
 
-    list: protectedProcedure.query(
-        async ({ ctx }) => {
-            return await ctx.prisma.event.findMany(
-                {
-                    where: {
-                        creatorId: ctx.session.user.id
-                    }
-                }
-            );
-        }
-    )
+  list: protectedProcedure.query(async ({ ctx }) => {
+    const data = await ctx.prisma.event.findMany({
+      where: {
+        creatorId: ctx.session.user.id,
+      },
+      include: {
+        _count: {
+          select: { attendees: true },
+        },
+      },
+    });
+  }),
 });
