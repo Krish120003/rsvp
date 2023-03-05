048ce675f7de30957f6d749168f7995d441e4dbe
Rachelle DeMan <demanr@mcmaster.ca>
2023-03-05 00:03:14 -0500

    chore: removed placeholder text

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -18,12 +18,12 @@ interface cardProps {
 const EventCard = (props: cardProps) => {
   const rsvps = props.attendees.length;
   return (
-    <div className="w-full bg-[#a26b6b] px-4 py-8">
+    <div className="w-full bg-[#dadada] px-4 py-8">
       <div className="flex scroll-m-20 justify-between px-4 align-middle text-xl tracking-tight">
-        <h2 className="">Event Name {props.name}</h2>
-        <h2 className="">X Views{props.description}</h2>
-        <h2 className="">X RSVPs{rsvps}</h2>
-        <h2 className="">Desc{props.description}</h2>
+        <h2 className="">{props.name}</h2>
+        <h2 className="">{props.description}</h2>
+        <h2 className="">{rsvps} RSVPs</h2>
+        <h2 className="">{props.description}</h2>
       </div>
     </div>
   );
