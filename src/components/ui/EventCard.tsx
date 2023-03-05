5151515fd0df9fa40ebff5ec13a1e8b693307a00
Rachelle DeMan <demanr@mcmaster.ca>
2023-03-04 23:57:27 -0500

    feat: added front end for event showing

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -7,20 +7,23 @@ interface Attendee {
   confirmed: Boolean;
   event: Event;
 }
-const EventCard = (
-  name: String,
-  description: String,
-  image: String,
-  attendees: Attendee[]
-) => {
-  const rsvps = attendees.length;
+
+interface cardProps {
+  name: String;
+  description: String;
+  image: String;
+  attendees: Attendee[];
+}
+
+const EventCard = (props: cardProps) => {
+  const rsvps = props.attendees.length;
   return (
     <div className="w-full bg-[#a26b6b] px-4 py-8">
       <div className="flex scroll-m-20 justify-between px-4 align-middle text-xl tracking-tight">
-        <h2 className="">Event Name {name}</h2>
-        <h2 className="">X Views{description}</h2>
+        <h2 className="">Event Name {props.name}</h2>
+        <h2 className="">X Views{props.description}</h2>
         <h2 className="">X RSVPs{rsvps}</h2>
-        <h2 className="">Desc{description}</h2>
+        <h2 className="">Desc{props.description}</h2>
       </div>
     </div>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,9 +7,10 @@ import Balancer from "react-wrap-balancer";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import { ModeToggle } from "@/components/mode-toggle";
-// import { Event } from "@/components/ui/Event";
+import EventCard from "@/components/ui/EventCard";
 
 const Dashboard: NextPage = () => {
+  const { data: session } = useSession();
   return (
     <>
       <Head>
@@ -23,12 +24,20 @@ const Dashboard: NextPage = () => {
           <Button onClick={() => signIn()}>Create Event</Button>
         </header>
         <div className="h-max w-full px-8 py-4">
-          <h1 className="text-2xld pt-6 font-bold lg:text-4xl">Your Events</h1>
-          <p className="flex-box w-full py-2 text-xl text-[#6F6F6F] dark:text-slate-400">
+          <h1 className="scroll-m-20 pt-6 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Your Events
+          </h1>
+          <p className="flex-box w-full py-2 text-xl text-slate-700 dark:text-slate-400">
             Welcome to your event dashboard. View your current, drafted, and
             archived events.
           </p>
           <div className="py-16">
+            <EventCard
+              name="string"
+              description="EE"
+              image="S"
+              attendees={[]}
+            />
             <p className="flex-box w-full justify-center pt-4 pb-4 text-center align-middle text-xl text-[#6F6F6F] dark:text-slate-400">
               You do not have any events right now. Create one to get started.
             </p>
